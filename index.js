d8cf218dbe1d465db3b5a6d9c8791f8ae60e9514
Aditya Trivedi <atadityatrivedi9@gmail.com>
2023-03-24 19:32:33 +0530

    added port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const PORT=process.env.PORT || 5173
+const PORT=process.env.PORT || 5000
 const { Server } = require("socket.io");
 app.use(cors());
 
